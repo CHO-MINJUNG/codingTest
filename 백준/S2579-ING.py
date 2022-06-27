c8a007f0e0ba146605b3f685a9e2a045d19ebdf2
MINJUNG-CHO <61917990+CHO-MINJUNG@users.noreply.github.com>
2022-06-27 14:11:20 +0900

    0627

--- 백준/S2579-ING.py
+++ 백준/S2579-ING.py
@@ -10,19 +10,16 @@ arr = [0]*cnt
 for i in range(cnt):
     arr[cnt-i-1] = int(input())
 
-result = arr[0]
-idx = 1
-while True:
-    if idx>len(arr)-1 :
-        break
-    if idx == len(arr)-1:
-        result += arr[idx]
-        break
-    if arr[idx] > arr[idx+1]:
-        result += arr[idx]
-        idx +=2
-    else:
-        result+= arr[idx+1]
-        idx+=2
+result = [[0]*(cnt) for _ in range(2)]
 
+if cnt>2:
+    result[0][0], result[1][0] = arr[0], arr[0]
+    result[0][1] = arr[0]+arr[1]
+    result[1][2] = arr[0]+arr[2]
+    for i in range(3, cnt):
+        result[0][i] = arr[i]+result[1][i-1]
+        result[1][i] = arr[i]+max(result[0][i-2], result[1][i-2])
+    print(max(map(max, result)))
+else:
+    print(sum(arr))
 print(result)
