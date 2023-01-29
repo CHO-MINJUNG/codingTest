1e9f6660f36e7e552d62ef09ceb4afa356188f2d
AD0559 <ad0559@AD0559ui-MacBookPro.local>
2023-01-29 13:01:34 +0900

    230129

--- 백준/G9019.py
+++ 백준/G9019.py
@@ -1,56 +1,50 @@
 # acmicpc.net/problem/9019
+from collections import deque
 
-def double(num):
-    return 10000%(num*2)
-
-def subtract(num):
-    if num==0:
-        return 9999
-    else :
-        return num-1        
-
-def leftRotate(num):
-    if len(str(num)) < 4 :
-        num = ('0'*(4-len(str(num))))+str(num)
-    l = list(str(num))
-    a = l.pop(0)
-    l.append(a)
-    return(int(''.join(l)))
-
-def rightRotate(num):
-    if len(str(num)) < 4 :
-        num = ('0'*(4-len(str(num))))+str(num)
-    l = list(str(num))
-    a = l.pop()
-    l.insert(0, a)
-    return(int(''.join(l)))
 
 cnt = int(input())
 for i in range(cnt):
-    a, b = list(map(int, input().split()))
-    testCase = []
-    result = []
-
-    testCase.append(double(a))
-    result.append('D')
-    testCase.append(subtract(a))
-    result.append('S')
-    testCase.append(leftRotate(a))
-    result.append('L')
-    testCase.append(rightRotate(a))
-    result.append('R')
-
+    a, b = map(int, input().split())
+    testCase = deque()
+    testCase.append([a,''])
+    visited = [0]*10001
     while True:
-        if testCase.pop(0) == b:
-            print(result.pop(0))
+        now = testCase.popleft()
+        nowNum = now[0]
+        nowResult = now[1]
+
+        if nowNum == b:
+            print(nowResult)
             break
-        nowNum = testCase.pop(0)
-        nowResult = result.pop(0)
-        testCase.append(double(nowNum))
-        result.append(nowResult+'D')
-        testCase.append(subtract(nowNum))
-        result.append(nowResult+'S')
-        testCase.append(leftRotate(nowNum))
-        result.append(nowResult+'L')
-        testCase.append(rightRotate(nowNum))
-        result.append(nowResult+'R')
+
+        
+        if nowNum*2>9999:
+            double = (nowNum*2)%10000
+        else:
+            double =  nowNum*2
+        if visited[double] == 0:
+            testCase.append([double, nowResult+'D'])
+            visited[double] = 1
+
+        if nowNum == 0:
+            subt=9999
+        else:
+            subt=nowNum-1
+
+        if visited[subt]==0:
+            testCase.append([subt, nowResult+'S'])
+            visited[subt]=1
+
+        c = nowNum%1000
+        d = nowNum//1000
+        left = c*10 + d
+        if visited[left]==0:
+            testCase.append([left, nowResult+'L'])
+            visited[left]=1
+        
+        c = nowNum%10
+        d = nowNum//10
+        right = c*1000 + d
+        if visited[right]==0:
+            testCase.append([right, nowResult+'R'])
+            visited[right]=1
